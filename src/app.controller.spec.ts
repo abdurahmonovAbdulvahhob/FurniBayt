c89777dfaa0f527308f9de66d1048b9facc3b454
mushtariy93 <abdurashidovamushtariykumush@gmail.com>
2025-01-14 08:46:46 +0500

    O'zgartirish haqida xabar

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,7 +15,7 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
+    it('should return "Salom dunyo"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
   });
