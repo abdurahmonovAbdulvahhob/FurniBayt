0ac38bd474776a451ee27d48da9f4f1e025fcb9b
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2025-01-20 11:35:45 +0500

    added winston logger

--- src/common/helpers/winston.logger.ts
+++ src/common/helpers/winston.logger.ts
@@ -1,23 +1,24 @@
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston';
 import * as winston from 'winston';
 
 export const winstonConfig = {
   transports: [
-    // new winston.transports.Console({
-    //   format: winston.format.combine(
-    //     winston.format.timestamp(),
-    //     nestWinstonModuleUtilities.format.nestLike(),
-    //   ),
-    // }),
-    // new winston.transports.File({
-    //   filename: 'logs/application.log',
-    //   level: 'info',
-    //   format: winston.format.combine(
-    //     winston.format.timestamp(),
-    //     winston.format.json(),
-    //   ),
-    // }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        nestWinstonModuleUtilities.format.nestLike(),
+      ),
+    }),
     new winston.transports.File({
-      filename: 'logs/error.log',
+      filename: 'application.log',
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    new winston.transports.File({
+      filename: 'error.log',
       level: 'error',
       format: winston.format.combine(
         winston.format.timestamp(),
